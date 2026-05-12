130c27c290ef5268456847fd53d93daccf3d31a7
Vianca <saime.vi@hotmail.com>
2026-05-12 12:08:53 +0200

    Bookedreseration Text modification

--- src/main/java/se/yrgo/client/BookaReservation.java
+++ src/main/java/se/yrgo/client/BookaReservation.java
@@ -9,14 +9,18 @@ import se.yrgo.service.CustomerService;
 import se.yrgo.service.ReservationService;
 import se.yrgo.service.TableService;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BookaReservation {
 
-    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("MM-dd");
 
     public static void BookaTable(Customer customer, CustomerService customerService, Scanner input, TableService tableService,
             ReservationService reservationService) {
@@ -40,6 +44,7 @@ public class BookaReservation {
         while (true) {
             System.out.println("Choose table id:");
             Long choice = input.nextLong();
+            input.nextLine();
 
             tables = tableService.findById(choice);
 
@@ -51,15 +56,46 @@ public class BookaReservation {
         }
 
         // date
-        System.out.print("Date and time (yyyy-MM-dd HH:mm): ");
-        LocalDateTime dateTime = null;
-        while (dateTime == null) {
+        System.out.print("Enter date (yyyy-MM-dd): ");
+
+        LocalDate date = null;
+
+        while (date == null) {
             try {
-                dateTime = LocalDateTime.parse(input.nextLine(), FMT);
+                date = LocalDate.parse(input.nextLine());
+
+                if (date.isBefore(LocalDate.now())) {
+                    System.out.println("Cannot choose past dates.");
+                    date = null;
+                }
+
             } catch (DateTimeParseException e) {
-                System.out.println("Invalid format, try again (yyyy-MM-dd HH:mm):");
+                System.out.println("Invalid format.");
             }
         }
+
+        //Tid
+        LocalTime start = LocalTime.of(17, 0);
+        LocalTime end = LocalTime.of(22, 0);
+
+        List<LocalTime> availableTimes = new ArrayList<>();
+
+        while (!start.isAfter(end)) {
+            availableTimes.add(start);
+            start = start.plusMinutes(30);
+        }
+
+        System.out.println("Available times: ");
+        for(int i = 0; i < availableTimes.size(); i++){
+            System.out.println((i + 1) + ". " + availableTimes.get(i));
+        }
+
+        int timeChoice = input.nextInt();
+        input.nextLine();
+
+        LocalTime chosenTime = availableTimes.get(timeChoice - 1);
+        LocalDateTime dateTime =
+                LocalDateTime.of(date, chosenTime);
 
         // Number of guests
         System.out.println("Number of guests ");
@@ -74,7 +110,7 @@ public class BookaReservation {
         try {
             Reservation r = reservationService.bookTable(
                     customer.getId(), tables.getId(), dateTime, guests, notes);
-            System.out.println("your table is booked!/n" + r);
+            System.out.println("Ditt bord är bokad " + r);
         } catch (IllegalArgumentException e) {
             System.out.println("could not book: " + e.getMessage());
         }
